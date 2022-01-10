46d2c82f83402427ad89538ec38f63cd63ca81ca
Lewis Gentle <lewis.gentle@gmail.com>
2022-01-10 12:17:36 +1100

    begin work on integrating itunes API

---
+++ allcasts/itunes_API.py
@@ -0,0 +1,17 @@
+''' itunes_API.py
+
+	This file contains the functions that interact with the iTunes API for searching for podcasts.	
+'''
+
+class ItunesAPI():
+	'''
+	
+	'''
+
+	def podcast_search():
+		'''
+		search for a podcast by name and return a cleaned dictionary of the results
+		podcasts[index]['name']
+		podcasts[index]['url']
+		podcasts[index]['description']
+		'''
